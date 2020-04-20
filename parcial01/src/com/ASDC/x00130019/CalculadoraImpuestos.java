f2b4fdf0e170a178a83dab4e57dcb3b8039100a2
Panchamul <andreachamul@gmail.com>
2020-04-19 23:41:07 -0600

    Final

--- parcial01/src/com/ASDC/x00130019/CalculadoraImpuestos.java
+++ parcial01/src/com/ASDC/x00130019/CalculadoraImpuestos.java
@@ -1,21 +1,53 @@
 package com.ASDC.x00130019;
 
-public static class CalculadoraImpuestos {
+public class CalculadoraImpuestos extends Empleado{
     private static double totalRenta;
-    private static double totalISS;
+    private static double totalISSS;
     private static double totalAFP;
 
-    CalculadoraImpuestos(){
 
+    CalculadoraImpuestos(String nombre, String puesto, double salario){
+        super(nombre, puesto, salario);
     }
 
-    public static double calcularPago(Empleado){
+    public static double calcularPago(Empleado empleado){
+    double res = 0, x = 0, pago =0;
+    if(empleado instanceof PlazaFija){
+        totalAFP = empleado.salario * 0.0625;
+        totalISSS = empleado.salario * 0.03;
+        res = empleado.salario-totalAFP-totalISSS;
+        x = res;
 
-
+        if(x <= 472)
+            empleado.salario = x;
+        else if(x >= 472.01 && x <= 895.24)
+            totalRenta = 0.1 * (x-472)+17.67;
+        else if(x >= 895.25 && x <= 2038.10)
+            totalRenta = 0.2 * (x-895.24)+60;
+        else if(x >= 2038.11) {
+            totalRenta = 0.3 * (x - 2038.10) + 288.57;
+        }
+        pago = res - totalRenta;
+    }else if(empleado instanceof ServicioProfesional){
+        totalRenta = 0.1 * empleado.salario;
+        pago = res-totalRenta;
     }
+       return pago;
+   }
 
     public static String mostrarTotales(){
+       return "Total Renta: "+ totalRenta + ", total de AFP: " + totalAFP + " y total del ISSS: " + totalISSS;
+   }
 
-
+    @Override
+    public String toString() {
+        return "CalculadoraImpuestos{" +
+                "totalRenta=" + totalRenta +
+                ", totalISSS=" + totalISSS +
+                ", totalAFP=" + totalAFP +
+                ", nombre='" + nombre + '\'' +
+                ", puesto='" + puesto + '\'' +
+                ", salario=" + salario +
+                '}';
     }
 }
--- parcial01/src/com/ASDC/x00130019/Main.java
+++ parcial01/src/com/ASDC/x00130019/Main.java
@@ -1,8 +1,74 @@
 package com.ASDC.x00130019;
 
+import javax.swing.*;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        String nombre, puesto;
+        int meses, extension;
+        double salario;
+        char op;
+        PlazaFija pf = null;
+        ServicioProfesional sp = null;
+        DecimalFormat decimales = new DecimalFormat("0.00");
+        Empleado aux = null;
+
+
+        ArrayList<ServicioProfesional> empleado1 = new ArrayList<>();
+        ArrayList<PlazaFija> empleado2 = new ArrayList<>();
+
+        do { Menu(); op = scan.next().charAt(0);
+            switch (op){
+                case '1':
+                    System.out.println("Servicio Profesional");
+                    nombre = JOptionPane.showInputDialog(null, "Nombre: ");
+                    puesto = JOptionPane.showInputDialog(null, "Puesto: ");
+                    try {
+                        salario = Double.parseDouble(JOptionPane.showInputDialog(null,"Salario: "));
+                    }catch (Exception e){
+                        JOptionPane.showMessageDialog(null, "Ingrese unicamente numeros");
+                        break;
+                    }
+                    if (salario < 250) {
+                        JOptionPane.showMessageDialog(null, "El salario no puede ser menor al minimo");
+                        break;
+                    }
+                    meses = Integer.parseInt(JOptionPane.showInputDialog(null,"Cantidad de meses a trabajar: "));
+                    empleado1.add(new ServicioProfesional(nombre,puesto,salario,meses));
+                            System.out.println(CalculadoraImpuestos.calcularPago(aux));
+
+
+                case '2':
+                    System.out.println("Plaza Fija");
+                    nombre = JOptionPane.showInputDialog(null, "Nombre: ");
+                    puesto = JOptionPane.showInputDialog(null, "Puesto: ");
+                    try {
+                        salario = Double.parseDouble(JOptionPane.showInputDialog(null,"Salario: "));
+                    }catch (Exception e){
+                        JOptionPane.showMessageDialog(null, "Ingrese unicamente numeros");
+                        break;
+                    }
+                    if (salario < 250) {
+                        JOptionPane.showMessageDialog(null, "El salario no puede ser menor al minimo");
+                        break;
+                    }
+                    extension = Integer.parseInt(JOptionPane.showInputDialog(null,"Extension: "));
+                    empleado2.add(new PlazaFija(nombre,puesto,salario,extension));
+                    System.out.println(CalculadoraImpuestos.calcularPago(aux));
+            }
+        }while (op != 3);
+
+
+    }
+
+    static void Menu(){
+        System.out.println("Bienvenido!!");
+        System.out.println("Seleccione el tipo de trabajo que posee");
+        System.out.println("1. Servicio Profesional");
+        System.out.println("2. Plaza fija");
     }
 }
